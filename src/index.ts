385c529fbb48af1cd23965ba1e6d74c1c5c6eaa5
Ayyan Shaikh <ayyan.sameer.shaikh.eren@gmail.com>
2023-09-24 15:37:22 +0530

    change index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ dotenv.config({ path: "/home/Ayyan/Desktop/RetroRevamp/server/.env" });
 import passport from "passport";
 import cookieSession from "cookie-session";
 import Razorpay from "razorpay";
+import path from "path";
 
 //Routes
 import authRoutes from "./routes/authRoutes";
@@ -50,13 +51,12 @@ if (process.env.NODE_ENV === "production") {
   //Express will serve up production assets
   //like our main.js file or main.css file
 
-  app.use(express.static("client/build"));
+  app.use(express.static("client/build  "));
 
   //Express will serve up the index.html file
   //if it doesn;t recognize the route
-  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "app/client", "build", "index.html"));
   });
 }
 //Routes Middleware
