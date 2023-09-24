c5991b8d94915bd5f372d9b7061dbf9cff92536f
Ayyan Shaikh <ayyan.sameer.shaikh.eren@gmail.com>
2023-09-24 15:53:37 +0530

    change index.ts

--- src/index.ts
+++ src/index.ts
@@ -51,12 +51,12 @@ if (process.env.NODE_ENV === "production") {
   //Express will serve up production assets
   //like our main.js file or main.css file
 
-  app.use(express.static("client/build  "));
+  app.use(express.static("client/build"));
 
   //Express will serve up the index.html file
   //if it doesn;t recognize the route
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve( "/app/client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 //Routes Middleware
